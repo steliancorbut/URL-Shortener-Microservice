e1f351a559d9c15570ca085e1f527375d9e8fd5c
Stelian Corbut <stelian@cdsromania.ro>
2019-07-30 16:30:53 +0300

    updates

--- server.js
+++ server.js
@@ -1,42 +1,78 @@
 'use strict';
 
-var express = require('express');
-var mongo = require('mongodb');
-var mongoose = require('mongoose');
+const express = require('express');
+const mongo = require('mongodb');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
-var cors = require('cors');
-var bodyParser = require('body-parser');
+const urlShort = require('./bin/validate');
+const app = express();
 
-var urlHandler = require('./controllers/urlHandler.js');
+const { URL } = require('url');
 
-var app = express();
+const urlDB = process.env.MONGO_URI;
 
-// Basic Configuration for Heroku
-var mongoURL = process.env.MONGOLAB_URI;
+const urlSave = require('./model/db');
+
+
+
+ 
+mongoose.connect(urlDB || 'mongodb://localhost:27017/shortUrls',{ useNewUrlParser: true });
+
+
+// Basic Configuration 
 var port = process.env.PORT || 3000;
 
-mongoose.connect(mongoURL);
-
 app.use(cors());
-app.use(bodyParser.urlencoded({'extended': false}));
 
+
+//logger
+app.use('/', (req, res, next) => {
+  console.log(`${req.method}, ${req.path} - ${req.ip}`);
+  next();
+});
+app.use(bodyParser.urlencoded({extended: false}));
 app.use('/public', express.static(process.cwd() + '/public'));
 
+
+app.get('/api/shorturl/all', (req,res) => {
+  urlSave.findAll(req, res);
+});
+app.get('/api/shorturl/:url', (req, res) => {
+  urlSave.getLongUrl(req.params.url, function(err, data) {
+    if (err) {
+      res.send('ERROR');
+    } else {
+      res.redirect(data);
+    }
+  });
+});
+
+
+app.post('/api/shorturl/new', function (req, res) {
+  const newLongUrl = req.body.url;
+  urlShort.validateUrl(newLongUrl, function (err, url) {
+    if (err) {
+      console.error("Error: Url didn't validate:");
+      console.error(err);      
+      res.json({error: 'invalid url'});      
+    } else {      
+      urlSave.createNew(url, function(err, shortUrl) {
+        if (err) {
+          console.error('Error: fail with database');
+          console.error(err);
+          res.send('Error with database.');
+        } else {
+          res.json({original_url: url, short_url: shortUrl});
+        }
+      });
+    }
+  });
+});
 app.get('/', function(req, res){
   res.sendFile(process.cwd() + '/views/index.html');
 });
-
-app.post('/api/shorturl/new', urlHandler.addUrl);
-  
-app.get('/api/shorturl/:shurl', urlHandler.processShortUrl);
-
-
-// Answer not found to all the wrong routes
-app.use(function(req, res, next){
-  res.status(404);
-  res.type('txt').send('Not found');
-});
-
 
 app.listen(port, function () {
   console.log('Node.js listening ...');
